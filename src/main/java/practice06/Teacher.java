03f2c515451eab80f992b5a8c2b21beb3cbee207
Jedidiah Ysamm Caychingco <jedidiah.ysamm.caychingco@oocl.com>
2019-09-20 09:48:31 +0800

    Finish practice06

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,30 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Integer klass;
+
+    public Teacher(String name, int age) {
+        this(name, age, null);
+    }
+
+    public Teacher(String name, int age, Integer klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        return String.format("%s I am a Teacher. I teach %s.",super.backIntroduce(), getKlassMessage());
+    }
+
+    private String getKlassMessage() {
+        if(klass == null){
+            return "No Class";
+        }
+        return "Class " + klass;
+    }
 }
