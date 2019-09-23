d6ad6a447c4e280b5f2bdc44014e04fa28894aea
Jedidiah Ysamm Caychingco <jedidiah.ysamm.caychingco@oocl.com>
2019-09-23 11:23:00 +0800

    Finish practice 08

--- src/main/java/practice08/Student.java
+++ src/main/java/practice08/Student.java
@@ -1,4 +1,30 @@
 package practice08;
 
-public class Student {
+import java.util.Optional;
+
+public class Student extends HasOccupation {
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age, klass);
+    }
+
+    @Override
+    protected String occupation() {
+        return "Student";
+    }
+
+    @Override
+    protected String specificDescription() {
+        if (isLeader()) {
+            return String.format("I am Leader of Class %d", klass.getNumber());
+        }
+        return String.format("I am at Class %d", klass.getNumber());
+    }
+
+    private boolean isLeader() {
+        return Optional.ofNullable(klass)
+                .map(Klass::getLeader)
+                .map(Student::getName)
+                .map(name::equals)
+                .orElse(false);
+    }
 }
